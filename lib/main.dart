2c03046227e89599ae7e268074344c128ccaa3d5
anshu singh <anshukit2011@gmail.com>
2021-08-19 19:59:59 +0530

    day6 complited

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
       initialRoute: "/",
       routes: {
         "/": (context) => LoginPage(),
-        MyRoutes.homeRoute:(context) => HomePage(),
+        MyRoutes.homeRoute: (context) => HomePage(),
         MyRoutes.loginRoute: (context) => LoginPage(),
       },
     );
